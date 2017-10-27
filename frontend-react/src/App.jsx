e83c6b34903a78e615c4c96002eb88da6f82d5b9
Angela Yi-Chun Chuang <angorayc@gmail.com>
2017-10-27 15:52:20 +0100

    add load more button and fetch results

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import People from './people'
+import Header from './header'
 import './styles.scss'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <People />
+        <Header />
+        <div>
+          <People />
+        </div>
       </div>
     )
   }
--- frontend-react/src/people/index.jsx
+++ frontend-react/src/people/index.jsx
@@ -6,26 +6,35 @@ class People extends Component {
   constructor(props) {
     super(props)
     this._fetchUsers = this._fetchUsers.bind(this)
+    this._handleLoadMoreClicked = this._handleLoadMoreClicked.bind(this)
     this.state = {
-      users: [],
-      info: {}
+      users: []
     }
   }
 
   _fetchUsers(results = 12) {
+    let currentUsers = this.state.users
     fetch(`https://randomuser.me/api/?results=${results}`, {mode: 'cors'})
       .then((response) => {
         return response.json()
       })
       .then((result) => {
+        let results = _get(result, 'results', [])
+        
+        currentUsers = currentUsers.concat(results)
+        console.log(results, currentUsers)
         this.setState({
-          users: _get(result, 'results', []),
-          info: _get(result, 'info', {})
+          users: currentUsers
         })
       })
       .catch(function(err) {
         console.log(err)
       })
+  }
+
+  _handleLoadMoreClicked(e) {
+    e.preventDefault()
+    this._fetchUsers()
   }
 
   componentDidMount() {
@@ -36,10 +45,13 @@ class People extends Component {
     let { users } = this.state
 
     return (
-      <div className="people">
-        {
-          users.map((user, i) => <User data={user} key={`user-${i}`} />)
-        }
+      <div>
+        <div className="people">
+          {
+            users.map((user, i) => <User data={user} key={`user-${i}`} />)
+          }
+          <button onClick={this._handleLoadMoreClicked} className="people__fetch">Load More</button>
+        </div>
       </div>
     )
   }
